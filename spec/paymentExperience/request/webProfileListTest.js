bcbd61be776c19c4912c945a48a5b95df4c46a35
jaypatel512 <japatel@paypal.com>
2018-01-16 14:49:25 -0600

    Updated Payment Experience API and Tests

--- spec/paymentExperience/request/webProfileListTest.js
+++ spec/paymentExperience/request/webProfileListTest.js
@@ -2,14 +2,14 @@
 
 const chai = require('chai');
 const client = require('../../test_harness').client();
-const WebProfileGetListRequest = require('../../../lib/paypalrestsdk').WebProfileGetListRequest;
+const WebProfileListRequest = require('../../../lib/paypalrestsdk').WebProfileListRequest;
 const WebProfileCreate = require('./webProfileCreateTest').WebProfileCreate;
 
 
-describe('WebProfileGetListRequest', function () {
+describe('WebProfileListRequest', function () {
   it('works as expected', function () {
     return WebProfileCreate().then((r) => {
-        let request = new WebProfileGetListRequest();
+        let request = new WebProfileListRequest();
 
         return client.execute(request).then((r) => {
           chai.assert.equal(r.statusCode, 200);
